45a247ba5fdd2a4cd7257aa66bca3a310660b205
GuelaKais <koroyeldiores@gmail.com>
2022-05-13 13:29:02 +0200

    automated update

--- projectsUpdater.sh
+++ projectsUpdater.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #script to automate project updating and pushing to github.com
 #I start from inside the bashProject directory
+# wok in progress
 token=$(cat ~/token.txt)
 cd ..
 variable=$( echo $(ls))
@@ -8,13 +9,10 @@ variable=$( echo $(ls))
 for i in $( echo $(ls))
 do
     cd $i
-    #git init #to init the repository
     git add *
     git commit -m "automated update"
-    #git branch -m main
-    #git remote add origin https://github.com/Guelakais/$i.git # to add the source of the github repo
-    git remote origin https://Guelakais:$token@github.com/Guelakais/$i.git
+    git branch -m main
     git push push -u origin main
-    git pull  origin main $i # downloads from origin branch
+    git pull  origin main
     cd ..
 done
